9cbb8fe37caed158a5ff04e5e39f964e78ac2d41
Dang Hoang Tuan <tsukii@disroot.org>
2023-01-13 15:07:01 +0700

    (main.py) Fix subcommand `ply` to use local envs instead of global_env

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ Ekam's CLI
 
 import click
 from os import system
-from ekam import run, global_env
+from ekam import run
 
-def execute_task(task):
+def execute_task(env, task):
     # This is indexing hell
-    cmds = global_env[task][1]
+    cmds = env[task][1]
     for cmd in cmds:
         # If the command is actually not a command but another recipe
         # then we will run that recipe instead
         if cmd["t"] == 3:
-            execute_task(cmd["v"])
+            execute_task(env, cmd["v"])
         else:
             click.echo(cmd["v"])
             system(cmd["v"])
@@ -29,14 +29,15 @@ def main():
 def ply(task, tree, quiet):
     with open("Ekamfile", "r") as f:
         run(f.read(), tree, quiet)
+    env = eval(f.read())
     # If the name is in the environment and it is an actual task (aka a tuple)
     # then we can run it
-    if task in global_env and isinstance(global_env[task], tuple):
-        execute_task(task)
+    if task in env and isinstance(env[task], tuple):
+        execute_task(env, task)
     # If the task is an alias, then we simply execute the task name that
     # alias is associated with
-    elif task in global_env and global_env[task]["t"] == 6:
-        execute_task(global_env[task]["v"])
+    elif task in env and env[task]["t"] == 6:
+        execute_task(env, env[task]["v"])
     else:
         click.secho(f"No task named {task}", fg="red")
 
