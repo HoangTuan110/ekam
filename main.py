6d54aeb85725906e5b0a3f6c9b11902be0bf3120
Dang Hoang Tuan <tsukii@disroot.org>
2023-01-13 20:28:17 +0700

    (main.py) s/task/recipe/g

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from os import system
 import click
 import ekam
 
-def execute_task(env, task):
+def execute_recipe(env, recipe):
     # This is indexing hell
-    cmds = env[task][1]
+    cmds = env[recipe][1]
     for cmd in cmds:
         # If the command is actually not a command but another recipe
         # then we will ekam.run that recipe instead
         if cmd["t"] == 3:
-            execute_task(env, cmd["v"])
+            execute_recipe(env, cmd["v"])
         else:
             click.echo(cmd["v"])
             system(cmd["v"])
@@ -23,22 +23,22 @@ def main():
     pass
 
 @click.command()
-@click.argument("task")
+@click.argument("recipe")
 @click.option("--tree", default=False)
 @click.option("--quiet", default=True)
-def ply(task, tree, quiet):
+def ply(recipe, tree, quiet):
     with open("Ekamfile", "r") as f:
         _, env = ekam.run(f.read(), tree, quiet)
-    # If the name is in the environment and it is an actual task (aka a tuple)
+    # If the name is in the environment and it is an actual recipe (aka a tuple)
     # then we can ekam.run it
-    if task in env and isinstance(env[task], tuple):
-        execute_task(env, task)
-    # If the task is an alias, then we simply execute the task name that
+    if recipe in env and isinstance(env[recipe], tuple):
+        execute_recipe(env, recipe)
+    # If the recipe is an alias, then we simply execute the recipe name that
     # alias is associated with
-    elif task in env and env[task]["t"] == 6:
-        execute_task(env, env[task]["v"])
+    elif recipe in env and env[recipe]["t"] == 6:
+        execute_recipe(env, env[recipe]["v"])
     else:
-        click.secho(f"No task named {task}", fg="red")
+        click.secho(f"No recipe named {recipe}", fg="red")
 
 main.add_command(ply)
 
